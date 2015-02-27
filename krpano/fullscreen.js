1cc0df4a64dc1b2c6363967011994e08f44a235f
David Vieira <davidhsv@gmail.com>
2015-02-27 15:05:46 -0300

    testes com webvr

---
+++ krpano/fullscreen.js
@@ -0,0 +1,110 @@
+/*
+Native FullScreen JavaScript API
+-------------
+Assumes Mozilla naming conventions instead of W3C for now
+*/
+
+(function() {
+  var
+    fullScreenApi = {
+      supportsFullScreen: false,
+      isFullScreen: function() { return false; },
+      requestFullScreen: function() {},
+      cancelFullScreen: function() {},
+      fullScreenEventName: '',
+      prefix: ''
+    },
+    browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+
+  // check for native support
+  if (typeof document.cancelFullScreen != 'undefined') {
+    fullScreenApi.supportsFullScreen = true;
+  } else {
+    // check for fullscreen support by vendor prefix
+    for (var i = 0, il = browserPrefixes.length; i < il; i++ ) {
+      fullScreenApi.prefix = browserPrefixes[i];
+
+      if (typeof document[fullScreenApi.prefix + 'CancelFullScreen' ] != 'undefined' ) {
+        fullScreenApi.supportsFullScreen = true;
+
+        break;
+      }
+    }
+  }
+
+  // update methods to do something useful
+  if (fullScreenApi.supportsFullScreen) {
+    fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+
+    fullScreenApi.isFullScreen = function() {
+      switch (this.prefix) {
+        case '':
+          return document.fullScreen;
+        case 'webkit':
+          return document.webkitIsFullScreen;
+        default:
+          return document[this.prefix + 'FullScreen'];
+      }
+    }
+    fullScreenApi.requestFullScreen = function(el) {
+      return (this.prefix === '') ? el.requestFullScreen() : el[this.prefix + 'RequestFullScreen']();
+    }
+    fullScreenApi.cancelFullScreen = function(el) {
+      return (this.prefix === '') ? document.cancelFullScreen() : document[this.prefix + 'CancelFullScreen']();
+    }
+  }
+
+  // jQuery plugin
+  if (typeof jQuery != 'undefined') {
+    jQuery.fn.requestFullScreen = function() {
+
+      return this.each(function() {
+        var el = jQuery(this);
+        if (fullScreenApi.supportsFullScreen) {
+          fullScreenApi.requestFullScreen(el);
+        }
+      });
+    };
+  }
+
+  // export api
+  window.fullScreenApi = fullScreenApi;
+})();
+
+// html5 fullscreen api
+function requestfullscreen(div)
+{
+  var fs = ["requestFullscreen","mozRequestFullScreen","webkitRequestFullscreen","webkitRequestFullScreen","msRequestFullscreen"];
+
+  for (var i=0; i<fs.length; i++)
+  {
+    if( div[fs[i]] )
+    {
+      div[fs[i]]();
+      break;
+    }
+  }
+}
+
+function exitHandler()
+{
+
+  var fs = window.fullScreenApi.isFullScreen();
+    if (fs)
+    {
+        /* Run code on exit */
+        document.getElementById('viewersDiv').style.display = 'block';
+        document.getElementById('bannerpano').style.display = 'none';
+    } else {
+      document.getElementById('viewersDiv').style.display = 'none';
+      document.getElementById('bannerpano').style.display = 'block';
+    }
+}
+
+if (document.addEventListener)
+{
+    document.addEventListener('webkitfullscreenchange', exitHandler, false);
+    document.addEventListener('mozfullscreenchange', exitHandler, false);
+    document.addEventListener('fullscreenchange', exitHandler, false);
+    document.addEventListener('MSFullscreenChange', exitHandler, false);
+}
